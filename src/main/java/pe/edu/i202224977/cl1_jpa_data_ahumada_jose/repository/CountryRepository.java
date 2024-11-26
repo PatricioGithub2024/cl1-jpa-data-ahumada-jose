a6ae3b78c9d63c4a5ba269c6b879addf12c5d74a
PatricioGithub2024 <jose_ahumada156@hotmail.com>
2024-11-25 19:57:09 -0500

    examen finalizado 2

--- src/main/java/pe/edu/i202224977/cl1_jpa_data_ahumada_jose/repository/CountryRepository.java
+++ src/main/java/pe/edu/i202224977/cl1_jpa_data_ahumada_jose/repository/CountryRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import pe.edu.i202224977.cl1_jpa_data_ahumada_jose.entity.Country;
 
 public interface CountryRepository extends CrudRepository<Country, String> {
+
+  //cambio en el git
 }
